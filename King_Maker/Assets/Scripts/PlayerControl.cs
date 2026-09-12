9ea0630ff6437447ee34ef24586f931d1f6d90ae
DESKTOP-2OET9J4\rudxo <rudxo0318@naver.com>
2026-09-12 21:08:54 +0900

    Revert "0912 병합 스크립트 수정"
    This reverts commit fbd9004ce4db5475667e804283ec394ecd5bf4c5.

--- King_Maker/Assets/Scripts/PlayerControl.cs
+++ King_Maker/Assets/Scripts/PlayerControl.cs
@@ -18,7 +18,7 @@ public class PlayerControl : NetworkBehaviour
 
     [Header("Jump / Gravity")]
     [SerializeField] private float jumpHeight = 1.2f;
-    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float gravity = -19.6f;
 
     [Header("Look Settings")]
     [SerializeField] private float mouseSensitivity = 0.12f;
